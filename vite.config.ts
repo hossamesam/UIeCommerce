c10c59e29557d429fcd5f6f8562d949f7f0cd30d
hossamesam <hossamesam599@gmail.com>
2025-07-05 00:48:40 +0300

    Add predeploy and deploy scripts to package.json; set base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/UIeCommerce/', // استبدل REPO_NAME باسم الريبو على GitHub
   plugins: [
     tailwindcss()],
 })
