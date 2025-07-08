2cb9e28447bc0ecc46749b14ea6a49259fc45108
hossamesam <hossamesam599@gmail.com>
2025-07-08 15:28:08 +0300

    v1.1.2

--- src/pages/Shop/Shop.tsx
+++ src/pages/Shop/Shop.tsx
@@ -61,7 +61,7 @@ function Shop() {
     )
 }
 function Filters({
-    
+
     itemsPerPage,
     onItemsPerPageChange
 }: {
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/UIeCommerce/', // استبدل REPO_NAME باسم الريبو على GitHub
+  base: '/UIeCommerce', // استبدل REPO_NAME باسم الريبو على GitHub
   plugins: [
     tailwindcss()],
 })
